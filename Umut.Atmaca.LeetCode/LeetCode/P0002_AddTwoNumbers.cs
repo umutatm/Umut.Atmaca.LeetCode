88e8811313c5d978d97d78498fdc3f438b335569
Umut Atmaca <hell_raider89@hotmail.com>
2018-11-17 14:05:28 +0300

    problem 2 solved

--- Umut.Atmaca.LeetCode/LeetCode/P0002_AddTwoNumbers.cs
+++ Umut.Atmaca.LeetCode/LeetCode/P0002_AddTwoNumbers.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Numerics;
 using System.Text;
 
 namespace Umut.Atmaca.Tester.LeetCode {
@@ -10,11 +8,7 @@ namespace Umut.Atmaca.Tester.LeetCode {
 
             string firstNumber = GetNodeValue(l1);
             string secondNumber = GetNodeValue(l2);
-
-            //BigInteger sum = BigInteger.Add(new BigInteger(Encoding.ASCII.GetBytes(firstNumber)), new BigInteger(Encoding.ASCII.GetBytes(secondNumber)));
-            long sum = Convert.ToInt64(firstNumber) + Convert.ToInt64(secondNumber);
-            string sumStr = sum.ToString();
-
+            string sumStr = SumBigNumbers(firstNumber, secondNumber);
             ListNode sumNode = CreateNode(sumStr);
 
             return sumNode;
@@ -36,11 +30,31 @@ namespace Umut.Atmaca.Tester.LeetCode {
             return node;
         }
 
+        private string SumBigNumbers(string number1, string number2) {
+            var maxLen = Math.Max(number1.Length, number2.Length);
+            number1 = number1.PadLeft(maxLen, '0');
+            number2 = number2.PadLeft(maxLen, '0');
+
+            string sumStr = string.Empty;
+            int carryDigit = 0;
+            for (var i = maxLen - 1; i >= 0; i--) {
+                var digitSum = Convert.ToInt32(number1[i].ToString()) + Convert.ToInt32(number2[i].ToString()) + carryDigit;
+                var remainder = digitSum % 10;
+                carryDigit = digitSum / 10;
+                sumStr = remainder + sumStr;
+            }
+            if (carryDigit > 0) {
+                sumStr = carryDigit + sumStr;
+            }
+
+            return sumStr;
+        }
+
     }
 
     public class ListNode {
-        public int val;
-        public ListNode next;
+        public int val { get; set; }
+        public ListNode next { get; set; }
         public ListNode(int x) {
             val = x;
         }
